bfe9b4773df22bb6ea9480f71d18eb9a1a808c5d
codescalper <msfunbook@gmail.com>
2023-10-06 20:10:33 +0530

    Added zod validation

--- components/form/SignUp.tsx
+++ components/form/SignUp.tsx
@@ -8,56 +8,70 @@ import {
     CardFooter,
     CardHeader,
     CardTitle,
-  } from "@/components/ui/card"
+} from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { FaGithub, FaGoogle } from 'react-icons/fa';
 import { Label } from "@/components/ui/label"
+import * as z from "zod"
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+})
 
 export default function CreateAccount() {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: { email: '', password: '' },
+  })
+
   return (
     <div>
-    <Card>
-      <CardHeader className="space-y-1">
-        <CardTitle className="text-2xl">Create an account</CardTitle>
-        <CardDescription>
-          Enter your email below to create your account
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <div className="grid grid-cols-2 gap-6">
-          <Button variant="outline">
-            <FaGithub className="mr-2 h-4 w-4" />
-            Github
-          </Button>
-          <Button variant="outline">
-          <FaGoogle className="mr-2 h-4 w-4" />
-            Google
-          </Button>
-        </div>
-        <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <span className="w-full border-t" />
-          </div>
-          <div className="relative flex justify-center text-xs uppercase">
-            <span className="bg-background px-2 text-muted-foreground">
-              Or continue with
-            </span>
-          </div>
-        </div>
-        <div className="grid gap-2">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="xyz@example.com" />
-        </div>
-        <div className="grid gap-2">
-          <Label htmlFor="password">Password</Label>
-          <Input id="password" type="password" />
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button className="w-full">Create account</Button>
-      </CardFooter>
-    </Card>
+      <Card>
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl">Create an account</CardTitle>
+          <CardDescription>
+            Enter your email below to create your account
+          </CardDescription>
+        </CardHeader>
+        <form onSubmit={form.handleSubmit(values => console.log(values))}>
+          <CardContent className="grid gap-4">
+            <div className="grid grid-cols-2 gap-6">
+              <Button variant="outline">
+                <FaGithub className="mr-2 h-4 w-4" />
+                Github
+              </Button>
+              <Button variant="outline">
+                <FaGoogle className="mr-2 h-4 w-4" />
+                Google
+              </Button>
+            </div>
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  Or continue with
+                </span>
+              </div>
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" placeholder="xyz@example.com" {...form.register('email')} />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" {...form.register('password')} />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button type='submit' className="w-full">Create account</Button>
+          </CardFooter>
+        </form>
+      </Card>
     </div>
   )
 }
