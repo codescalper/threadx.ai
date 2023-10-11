3ce9212d62fcd3042fb613fc4914458b21c1a4c8
codescalper <msfunbook@gmail.com>
2023-10-11 17:51:46 +0530

    converted client side component to server side component

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -1,7 +1,23 @@
+import { authOptions } from "@/lib/auth";
 import React from "react";
+import { getServerSession } from "next-auth";
+import Header from "@/app/Header";
 
-function Generate() {
-  return <div>Generate</div>;
-}
+const Generate = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+  const emailName = session?.user?.email?.split("@")[0];
+
+  if (session?.user) {
+    return (
+      <>
+        <Header />
+        Welcome {emailName}
+      </>
+    );
+  }
+
+  return <div>Please login to see generate page</div>;
+};
 
 export default Generate;
