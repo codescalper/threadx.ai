ecddc5bd4202cfa8a8fadb44f3ed92447a140935
codescalper <msfunbook@gmail.com>
2023-12-09 14:53:34 +0530

    changes in rediret url

--- components/form/SignIn.tsx
+++ components/form/SignIn.tsx
@@ -1,71 +1,64 @@
-"use client"
+"use client";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { FaGithub, FaGoogle } from 'react-icons/fa';
-import { Label } from "@/components/ui/label"
-import * as z from "zod"
-import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { FaGithub, FaGoogle } from "react-icons/fa";
+import { Label } from "@/components/ui/label";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
-import {signIn} from "next-auth/react"
-import { useRouter } from 'next/navigation';
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 
 const formSchema = z.object({
   email: z.string().email(),
   password: z
     .string()
-    .min(8, 'Password should be greater than 8 characters')
+    .min(8, "Password should be greater than 8 characters")
     .regex(
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/,
-      'Password must contain at least one uppercase letter, one lowercase letter, and one numeric character'
+      "Password must contain at least one uppercase letter, one lowercase letter, and one numeric character"
     ),
 });
-
 
 export default function SignIn() {
   const router = useRouter();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-       email: '',
-       password: '',
+      email: "",
+      password: "",
     },
   });
 
   const onSubmit = async (value: z.infer<typeof formSchema>) => {
     console.log("onSubmit called");
-    const signInData = await signIn('credentials', {
+    const signInData = await signIn("credentials", {
       email: value.email,
       password: value.password,
       redirect: false,
-    })
+    });
 
-    if(signInData?.error) {
-      toast.error('Invalid credentials')
-      console.log(signInData.error)
-    }else{
-      router.push('/generate')
-      toast.success('Logged in successfully!')
-
+    if (signInData?.error) {
+      toast.error("Invalid credentials");
+      console.log(signInData.error);
+    } else {
+      router.push("/generate");
+      toast.success("Logged in successfully!");
     }
 
     // Success, show toast
-  
-   
-  }
- 
-
-  
+  };
 
   return (
     <div className="flex justify-center items-center h-screen">
@@ -79,11 +72,25 @@ export default function SignIn() {
         <form onSubmit={form.handleSubmit(onSubmit)}>
           <CardContent className="grid gap-4">
             <div className="grid grid-cols-2 gap-6">
-              <Button variant="outline" onClick={()=>signIn('github',{callbackUrl:'http://localhost:3000/generate'})}>
+              <Button
+                variant="outline"
+                onClick={() =>
+                  signIn("github", {
+                    callbackUrl: "https://threadx.10xeng.xyz/generate",
+                  })
+                }
+              >
                 <FaGithub className="mr-2 h-4 w-4" />
                 Github
               </Button>
-              <Button variant="outline" onClick={()=>signIn('google',{callbackUrl:'http://localhost:3000/generate'})}>
+              <Button
+                variant="outline"
+                onClick={() =>
+                  signIn("google", {
+                    callbackUrl: "https://threadx.10xeng.xyz/generate",
+                  })
+                }
+              >
                 <FaGoogle className="mr-2 h-4 w-4" />
                 Google
               </Button>
@@ -100,11 +107,20 @@ export default function SignIn() {
             </div>
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
-              <Input id="email" type="email" placeholder="xyz@example.com" {...form.register('email')} />
+              <Input
+                id="email"
+                type="email"
+                placeholder="xyz@example.com"
+                {...form.register("email")}
+              />
             </div>
             <div className="grid gap-2">
               <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" {...form.register('password')}  />
+              <Input
+                id="password"
+                type="password"
+                {...form.register("password")}
+              />
               {form.formState.errors.password && ( // Check if there's an error for the 'password' field
                 <p className="text-red-500 text-xs">
                   {form.formState.errors.password.message}
@@ -113,18 +129,18 @@ export default function SignIn() {
             </div>
           </CardContent>
           <CardFooter>
-            <Button type='submit' className="w-full">Sign In</Button>
-            
+            <Button type="submit" className="w-full">
+              Sign In
+            </Button>
           </CardFooter>
-          <p className='text-center text-sm mt-2'>
-              If you don&apos;t have an account, please&nbsp;
-              <Link className='text-orange-500 hover:underline' href='/sign-up'>
-                Sign up 
-              </Link>
-            </p>
+          <p className="text-center text-sm mt-2">
+            If you don&apos;t have an account, please&nbsp;
+            <Link className="text-orange-500 hover:underline" href="/sign-up">
+              Sign up
+            </Link>
+          </p>
         </form>
-      
       </Card>
     </div>
-  )
+  );
 }
--- components/form/SignUp.tsx
+++ components/form/SignUp.tsx
@@ -1,71 +1,68 @@
-"use client"
-import toast, { Toaster } from 'react-hot-toast';
-import { Button } from "@/components/ui/button"
+"use client";
+import toast, { Toaster } from "react-hot-toast";
+import { Button } from "@/components/ui/button";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { FaGithub, FaGoogle } from 'react-icons/fa';
-import { Label } from "@/components/ui/label"
-import * as z from "zod"
-import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { FaGithub, FaGoogle } from "react-icons/fa";
+import { Label } from "@/components/ui/label";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
-import {useRouter} from "next/navigation";
-import { signIn } from 'next-auth/react';
+import { useRouter } from "next/navigation";
+import { signIn } from "next-auth/react";
 
 const formSchema = z
-.object({
-  email: z.string().email(),
-  password: z
-    .string()
-    .min(8, 'Password should be greater than 8 characters')
-    .regex(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/,
-      'Password must contain at least one uppercase letter, one lowercase letter, and one numeric character'
-    ),
-    renteredPassword:z.string().min(8,"Password confirmation is required")
-})
-.refine((data)=>data.password===data.renteredPassword,{
-  path:['renteredPassword'],
-  message:"Password do not match"
-})
-
-
+  .object({
+    email: z.string().email(),
+    password: z
+      .string()
+      .min(8, "Password should be greater than 8 characters")
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/,
+        "Password must contain at least one uppercase letter, one lowercase letter, and one numeric character"
+      ),
+    renteredPassword: z.string().min(8, "Password confirmation is required"),
+  })
+  .refine((data) => data.password === data.renteredPassword, {
+    path: ["renteredPassword"],
+    message: "Password do not match",
+  });
 
 export default function SignUp() {
-
   const router = useRouter();
 
-const onSubmit= async (value:z.infer<typeof formSchema>) =>{
-    const response = await fetch('/api/user',{
-      method:"POST",
-      headers:{
-        'Content-Type':'application/json'
+  const onSubmit = async (value: z.infer<typeof formSchema>) => {
+    const response = await fetch("/api/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-      body:JSON.stringify({
-        email:value.email,
-        password:value.password
-      })
-    })
+      body: JSON.stringify({
+        email: value.email,
+        password: value.password,
+      }),
+    });
 
-    if(response.ok){
-      toast.success('Account created successfully')
-      router.push('/sign-in')
-    }else{
-      console.error("Registration failed")
+    if (response.ok) {
+      toast.success("Account created successfully");
+      router.push("/sign-in");
+    } else {
+      console.error("Registration failed");
     }
-}
-  
+  };
+
   const form = useForm({
     resolver: zodResolver(formSchema),
-    defaultValues: { email: '', password: '',renteredPassword:'' },
-  })
+    defaultValues: { email: "", password: "", renteredPassword: "" },
+  });
 
   return (
     <div className="flex justify-center items-center h-screen">
@@ -79,11 +76,25 @@ const onSubmit= async (value:z.infer<typeof formSchema>) =>{
         <form onSubmit={form.handleSubmit(onSubmit)}>
           <CardContent className="grid gap-4">
             <div className="grid grid-cols-2 gap-6">
-            <Button variant="outline" onClick={()=>signIn('github',{callbackUrl:'http://localhost:3000/generate'})}>
+              <Button
+                variant="outline"
+                onClick={() =>
+                  signIn("github", {
+                    callbackUrl: "https://threadx.10xeng.xyz/generate",
+                  })
+                }
+              >
                 <FaGithub className="mr-2 h-4 w-4" />
                 Github
               </Button>
-              <Button variant="outline" onClick={()=>signIn('google',{callbackUrl:'http://localhost:3000/generate'})}>
+              <Button
+                variant="outline"
+                onClick={() =>
+                  signIn("google", {
+                    callbackUrl: "https://threadx.10xeng.xyz/generate",
+                  })
+                }
+              >
                 <FaGoogle className="mr-2 h-4 w-4" />
                 Google
               </Button>
@@ -100,40 +111,52 @@ const onSubmit= async (value:z.infer<typeof formSchema>) =>{
             </div>
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
-              <Input id="email" type="email" placeholder="xyz@example.com" {...form.register('email')} />
+              <Input
+                id="email"
+                type="email"
+                placeholder="xyz@example.com"
+                {...form.register("email")}
+              />
             </div>
             <div className="grid gap-2">
               <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" {...form.register('password')}  />
+              <Input
+                id="password"
+                type="password"
+                {...form.register("password")}
+              />
               {form.formState.errors.password && ( // Check if there's an error for the 'password' field
                 <p className="text-red-500 text-xs">
                   {form.formState.errors.password.message}
                 </p>
               )}
               <Label htmlFor="renteredPassword">Re-enter your Password</Label>
-              <Input id="reenteredPassword" type="password" placeholder="Re-enter your Password" {...form.register('renteredPassword')}  />
-               {form.formState.errors.renteredPassword && (
-              <p className="text-red-500 text-xs">
-                {form.formState.errors.renteredPassword.message}
-              </p>
-)}
+              <Input
+                id="reenteredPassword"
+                type="password"
+                placeholder="Re-enter your Password"
+                {...form.register("renteredPassword")}
+              />
+              {form.formState.errors.renteredPassword && (
+                <p className="text-red-500 text-xs">
+                  {form.formState.errors.renteredPassword.message}
+                </p>
+              )}
             </div>
-
           </CardContent>
           <CardFooter>
-            <Button type='submit' className="w-full">Create account</Button>
-            
+            <Button type="submit" className="w-full">
+              Create account
+            </Button>
           </CardFooter>
-          <p className='text-center text-sm mt-2'>
-              Already have an account?, please&nbsp;
-              <Link className='text-orange-500 hover:underline' href='/sign-in'>
-                Sign in
-              </Link>
-            </p>
+          <p className="text-center text-sm mt-2">
+            Already have an account?, please&nbsp;
+            <Link className="text-orange-500 hover:underline" href="/sign-in">
+              Sign in
+            </Link>
+          </p>
         </form>
-      
       </Card>
-      
     </div>
-  )
+  );
 }
